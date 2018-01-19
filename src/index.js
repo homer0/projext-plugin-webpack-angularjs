66e6bac25d264886e603cd552bfd5151a7946636
homer0 <leo.apiwan@olapic.com>
2018-01-19 17:17:31 -0300

    Document all the code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const WoopackAngularJSPlugin = require('./plugin');
-
-module.exports = (app) => {
+/**
+ * This is the method called by Woopack when loading the plugin and it takes care of just creating
+ * a new instance of the plugin main class and using it to register on Woopack.
+ * @param {Woopack} app The Woopack main container.
+ * @ignore
+ */
+const loadPlugin = (app) => {
   const plugin = new WoopackAngularJSPlugin();
   plugin.register(app);
 };
+
+module.exports = loadPlugin;
--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,52 @@
+/**
+ * This service is in charge of adding the required loader to handle AngularJS inject annotations
+ * and updating a target Babel configuration so the loader will work.
+ */
 class WoopackAngularJSPlugin {
+  /**
+   * Class constructor.
+   * @ignore
+   */
   constructor() {
+    /**
+     * The name of the event reducer the service uses to reduce the loader configuration after
+     * modifying it.
+     * @type {String}
+     */
     this.eventName = 'webpack-js-loaders-configuration-for-browser';
+    /**
+     * The required value a target `framework` property needs to have in order for the plugin to
+     * take action.
+     * @type {String}
+     */
     this.frameworkProperty = 'angularjs';
+    /**
+     * The name of the loader the plugin adds to the Webpack configuration.
+     * @type {String}
+     */
     this.loaderName = 'ng-annotate-loader';
+    /**
+     * The name of the loader with the Babel configurations.
+     * @type {String}
+     */
     this.babelLoaderName = 'babel-loader';
+    /**
+     * The list of transformations the loader needs in order to work. The loader only works with
+     * standard `function` statements, so it's necessary for Babel to transpile the following things
+     * into `function`s so the loader can inject the annotations.
+     * @type {Array}
+     */
     this.babelRequiredIncludedFeatures = [
       'transform-es2015-arrow-functions',
       'transform-es2015-classes',
       'transform-es2015-parameters',
     ];
   }
-
+  /**
+   * This is the method called when the plugin is loaded by Woopack. It just gets the events service
+   * and registers a listener for the event reducer that handles JS loaders for browser targets.
+   * @param {Woopack} app The Woopack main container.
+   */
   register(app) {
     const events = app.get('events');
     events.on(
@@ -18,30 +54,54 @@ class WoopackAngularJSPlugin {
       (loaders, params) => this.updateLoaders(loaders, params.target)
     );
   }
-
+  /**
+   * This method gets called when Woopack reduces the JS loaders for browser targets. It
+   * validates the target, adds the loader and modifies, if necessary, the configuration for
+   * Babel.
+   * @param {Array}  currentLoaders The list of JS loaders for the Webpack configuration.
+   * @param {Target} target         The target information.
+   * @return {Array} The updated list of loaders.
+   */
   updateLoaders(currentLoaders, target) {
     let updatedLoaders;
+    /**
+     * If the target has a valid type, the right `framework` property and there are loaders
+     * to modify...
+     */
     if (
       target.is.browser &&
       target.framework === this.frameworkProperty &&
       currentLoaders.length
     ) {
+      // ...copy the list of loaders.
       updatedLoaders = currentLoaders.slice();
+      // Get the first loader of the list (there's usually only one).
       const [baseJSLoader] = updatedLoaders;
+      // Push the AngularJS loader as first on the list.
       baseJSLoader.use.unshift(this.loaderName);
+      // Get the index of the Babel loader.
       const babelLoaderIndex = this._findBabelLoaderIndex(baseJSLoader.use);
+      // If the Babel loader is present...
       if (babelLoaderIndex > -1) {
+        // ...replace it with an updated version.
         baseJSLoader.use[babelLoaderIndex] = this._updateBabelLoader(
           baseJSLoader.use[babelLoaderIndex]
         );
       }
     } else {
+      // ...otherwise, just set to return the received loaders.
       updatedLoaders = currentLoaders;
     }
 
     return updatedLoaders;
   }
-
+  /**
+   * Finds the index of the Babel loader in a list of loaders.
+   * @param {Array} loaders The list of loaders.
+   * @return {Number}
+   * @ignore
+   * @access protected
+   */
   _findBabelLoaderIndex(loaders) {
     return loaders.findIndex((loader) => {
       const isString = typeof loader === 'string';
@@ -49,53 +109,82 @@ class WoopackAngularJSPlugin {
         (!isString && loader.loader === this.babelLoaderName);
     });
   }
-
+  /**
+   * Updates an existing Babel loader configuration with the required transformations the
+   * AngularJS loader needs in order to work.
+   * The method will only modify the loader if is not on a string format, has an `options`
+   * object and, doesn't have any preset or has the `env` preset.
+   * @param {Object|String} babelLoader The loader to update.
+   * @return {Object|String}
+   * @ignore
+   * @access protected
+   */
   _updateBabelLoader(babelLoader) {
     let updatedLoader;
     /**
      * If the loader is a `string` or it doesn't have an `options` property, then the project uses
-     * an external `.babelrc`.
+     * an external `.babelrc`, so it won't be updated.
      */
     if (typeof babelLoader !== 'string' && babelLoader.options) {
+      // Access the loader options.
       const { options } = babelLoader;
+      // Define some validation flags.
       let needsPresets = false;
       let hasEnvPreset = false;
       let envPresetIndex = -1;
+      // If there are presets options...
       if (options.presets && options.presets.length) {
+        // ...find the index of the `env` preset.
         envPresetIndex = options.presets.findIndex((preset) => {
           const [presentName] = preset;
           return presentName === 'env';
         });
-
+        // Validate that the `env` preset is on the options.
         hasEnvPreset = envPresetIndex > -1;
       } else {
+        // ...otherwise, the options needs presets.
         needsPresets = true;
       }
-
+      // If there's an `env` preset to configure or no presets at all...
       if (needsPresets || hasEnvPreset) {
+        // ...start updating the options.
         let presetOptions;
+        // If there are no presets...
         if (needsPresets) {
+          // ...create an empty object to work with.
           presetOptions = {};
         } else {
+          // ...otherwise, get the current `env` preset configuration.
           const [, currentEnvPresetOptions] = options.presets[envPresetIndex];
+          // copy the configuration into a new reference.
           presetOptions = Object.assign({}, currentEnvPresetOptions);
         }
-
+        /**
+         * From this point forward, we assume we have a configuration for the `env` preset.
+         * If the configuration already has an `include` setting...
+         */
         if (presetOptions.include) {
+          // ...we only push the required transformations that are not already present.
           this.babelRequiredIncludedFeatures.forEach((feature) => {
             if (!presetOptions.include.includes(feature)) {
               presetOptions.include.push(feature);
             }
           });
         } else {
+          // ...we copy the list of required transformations.
           presetOptions.include = this.babelRequiredIncludedFeatures.slice();
         }
-
+        // Set the object to be returned as a copy of the one received.
         updatedLoader = Object.assign({}, babelLoader);
+        // Declare the new `env` preset.
+        const newEnvPreset = ['env', presetOptions];
+        // If it didn't have any presets...
         if (needsPresets) {
-          updatedLoader.options.presets = [['env', presetOptions]];
+          // ...create a new presets list with the new `env` preset.
+          updatedLoader.options.presets = [newEnvPreset];
         } else {
-          updatedLoader.options.presets[envPresetIndex] = ['env', presetOptions];
+          // ...otherwise, replace the existing `env` preset with the new and updated one.
+          updatedLoader.options.presets[envPresetIndex] = newEnvPreset;
         }
       } else {
         updatedLoader = babelLoader;
