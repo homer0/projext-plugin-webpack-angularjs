9c278e61ae766428eaa52f8d35c55970c9f4e46f
homer0 <leo.apiwan@olapic.com>
2018-01-19 23:20:02 -0300

    Fix documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const WoopackAngularJSPlugin = require('./plugin');
 /**
- * This is the method called by Woopack when loading the plugin and it takes care of just creating
+ * This is the method called by Woopack when loading the plugin and it takes care of creating
  * a new instance of the plugin main class and using it to register on Woopack.
  * @param {Woopack} app The Woopack main container.
  * @ignore
