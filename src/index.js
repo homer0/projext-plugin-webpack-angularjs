6eea83933d22280cadeca509723378d05bf0c2d9
homer0 <leo.apiwan@olapic.com>
2018-02-11 07:34:13 -0300

    Fix documentation typos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const WoopackAngularJSPlugin = require('./plugin');
 /**
- * This is the method called by Woopack when loading the plugin and it takes care of creating
- * a new instance of the plugin main class and using it to register on Woopack.
+ * This is the method called by Woopack when loading the plugin. It takes care of creating
+ * a new instance of the plugin class and use it to register for the required events.
  * @param {Woopack} app The Woopack main container.
  * @ignore
  */
